60c3d04a73edb23df33b865630bc04f0c30359e4
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2023-11-04 22:22:41 +0200

    adeed numbers to left and right

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,6 @@
 
 <h2 class="text-2xl font-bold">ORDX</h2>
 <section class="flex justify-between gap-2">
-    <div class="bg-red-600 w-2/3">XL</div>
-    <div class="bg-green-600">XR</div>
+    <div class="bg-red-600 w-2/3">XL:01</div>
+    <div class="bg-green-600">02:XR</div>
 </section>
