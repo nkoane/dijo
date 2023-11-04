f67fab8627ac2e1d30927cfa7858ef3a7b7dd9b4
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2023-11-04 22:14:59 +0200

    lets get this patch thing working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
     $: console.log(data);
 </script>
 
-<h2 class="text-2xl font-bold">ORDER</h2>
+<h2 class="text-2xl font-bold">ORDXR</h2>
 <section class="flex justify-between gap-2">
     <div class="bg-red-600 w-2/3">left</div>
     <div class="bg-green-600">right</div>
