2c2faf4e75a842708e6540a682416d8a13d9de64
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2023-11-04 17:38:55 +0200

    okay, we done playing.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<h2>?</h2>
+<script lang="ts">
+    export let data;
+
+    $: console.log(data);
+</script>
+
+<h2 class="text-2xl font-bold">ORDER</h2>
+<section class="flex justify-between gap-2">
+    <div class="bg-red-600 w-2/3">left</div>
+    <div class="bg-green-600">right</div>
+</section>
