027b3c2a82bacf4fcf64c23e457d869adf72961f
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2023-11-06 11:22:47 +0200

    changes before changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
     $: console.log(data);
 </script>
 
-<h2 class="text-2xl font-bold">ORDX</h2>
+<h2 class="text-2xl font-bold">ORDX  It is what it is?</h2>
 <section class="flex justify-between gap-2">
-    <div class="bg-red-600 w-2/3">XL:01</div>
+    <div class="bg-red-600 w-2/3">DL:01</div>
     <div class="bg-green-600">02:XR</div>
 </section>
 <footer>[the ticker]</footer>
