3b1cfd94f30deaba1b93cc0e5af1727e5c933a8a
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-02-24 10:35:42 +0200

    i do not know why the admin/layout does not show.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,4 @@
 	export let data;
 </script>
 
-<h2 class="mb-4 text-3xl font-bold">Hello, {data.user?.username ?? 'Stranger'}.</h2>
+<h2 class="mb-4 text-3xl font-bold">Hello, {data.user?.username ?? 'Stranger'}!</h2>
