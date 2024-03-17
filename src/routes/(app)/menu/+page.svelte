7192a6d66b3cfe9f2bc2c92d4269e3b767e53356
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-03-17 11:05:09 +0200

    okay, we have the menu-ui-ish sorted now lets get the functionality going.

--- src/routes/(app)/menu/+page.svelte
+++ src/routes/(app)/menu/+page.svelte
@@ -34,7 +34,7 @@
 		Starch: Wheat,
 		Meat: Beef,
 		Vegetables: LeafyGreen,
-		Drinks: GlassWater
+		Beverages: GlassWater
 	};
 
 	$: if (form?.order) {
@@ -47,30 +47,52 @@
 	$: console.log('(menu/page.svelte) -> menu', menu);
 </script>
 
-<h2>Menu</h2>
-
-<section id="menu">
-	{#each Object.keys(menu) as category}
-		<dl>
-			<dt class="border-b font-bold">{category}</dt>
-			<dd>
-				<ul class="list-decimal">
-					{#each menu[category] as foodCategories}
-						<li>
-							{foodCategories.name}
-							{#if foodCategories.description}
-								<p>{foodCategories.description}</p>
-							{/if}
-							{#if foodCategories.price}
-								<p>R{foodCategories.price}</p>
-							{/if}
-						</li>
+<h2 class="mb-12">Menu</h2>
+<div class="flex gap-4">
+	<section id="menu" class="flex-grow">
+		{#each Object.keys(menu) as category}
+			<dl class="bg-gray-50 p-4">
+				<dt class="mb-2 border-b border-t font-bold">{category}</dt>
+				<dd class="flex flex-wrap">
+					{#each menu[category] as food}
+						<ul class="mb-4 flex w-1/2 flex-grow justify-between gap-12 last:mb-0">
+							<li class="w-3/4 bg-blue-50">
+								<p>{food.name}</p>
+								<p>{food.description}</p>
+								<p>R{food.price}</p>
+							</li>
+							<li class="flex w-1/4 justify-between justify-self-center bg-pink-100">
+								<button type="submit" formaction="?/add"><SquareMinus /></button>
+								<button type="submit" formaction="?/add"><SquarePlus /></button>
+							</li>
+						</ul>
 					{/each}
-				</ul>
-			</dd>
-		</dl>
-	{/each}
-</section>
-<section id="order">
-	<!-- display order detaisl -->
-</section>
+				</dd>
+			</dl>
+		{/each}
+	</section>
+	<section id="order" class="w-3/12 flex-shrink-0 bg-pink-50">
+		<!-- display order detaisl -->
+		<form method="post">
+			<ul class="list-decimal">
+				{#each [1, 22, 44, 55] as foodId (foodId)}
+					<input type="hidden" name="foodId-{foodId}" />
+					<li class="border-b-2 border-white text-sm last:border-none">
+						<dl class="flex justify-between">
+							<dt>Food</dt>
+							<dd>x {Math.max(1, Math.round(Math.random() * 4))}</dd>
+							<dd>R {Math.round(Math.random() * 20) + 10}.00</dd>
+							<button type="submit" formaction="?/trash"><Trash /></button>
+						</dl>
+					</li>
+				{/each}
+			</ul>
+			<div class="flex justify-between">
+				<p>total: R {100.45}</p>
+				<p>paid: R {101.05}</p>
+				<p>change: R {0.55}</p>
+			</div>
+			<button type="submit" formaction="?/checkout"><SquareArrowRight /> place</button>
+		</form>
+	</section>
+</div>
