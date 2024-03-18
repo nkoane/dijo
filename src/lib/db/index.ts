49ee549a70acd90913828e9f4b19ec4e201d305c
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-03-18 10:03:23 +0200

    Add createTimeDifference function to calculate time difference

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -6,3 +6,33 @@ export type OrderDetail = Order & { items: OrderItemDetail[]; status: OrderStatu
 export type Orders = { [key: string]: OrderDetail[] };
 
 export type FoodMenu = { [key: string]: FoodDetail[] };
+
+export function createTimeDifference(
+	id: number,
+	date: Date
+): {
+	id: number;
+	date: Date;
+	diff: { seconds: number; minutes: number; hours: number; days: number; value: number };
+	words: string;
+} {
+	const diff = new Date().getTime() - new Date(date).getTime();
+	const seconds = Math.floor(diff / 1000);
+	const minutes = Math.floor(seconds / 60);
+	const hours = Math.floor(minutes / 60);
+	const days = Math.floor(hours / 24);
+	const words = `${days}d ${hours % 24}h ${minutes % 60}m ${seconds % 60}s`;
+
+	return {
+		id,
+		date,
+		diff: {
+			seconds,
+			minutes,
+			hours,
+			days,
+			value: diff
+		},
+		words
+	};
+}
