441e8aec3e29a986bf5ec1670e94b5cc9c28dc44
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-03-30 08:50:43 +0200

    listed the categories, and shown the form

--- src/routes/(dashboard)/admin/categories/+page.svelte
+++ src/routes/(dashboard)/admin/categories/+page.svelte
@@ -1,1 +1,49 @@
+<script lang="ts">
+	export let data;
+	const categories = data.categories;
+	$: {
+		console.log(data);
+	}
+</script>
+
 <h3>Categories</h3>
+<div class="flex justify-between">
+	<section>
+		{#if categories.length > 0}
+			<ol>
+				{#each categories as category}
+					<li>{category.name}</li>
+				{/each}
+			</ol>
+		{:else}
+			<p>No categories found</p>
+		{/if}
+	</section>
+	<form method="post">
+		<p>
+			<label for="name">name</label>
+			<input type="text" id="name" name="name" />
+		</p>
+		<p>
+			<label for="description">description</label>
+			<textarea name="description" id="description"></textarea>
+		</p>
+	</form>
+</div>
+
+<style lang="postcss">
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	form p label {
+		@apply ml-2 text-sm;
+	}
+
+	form p input,
+	form p textarea {
+		@apply w-full rounded-sm bg-gray-100 p-2;
+	}
+</style>
