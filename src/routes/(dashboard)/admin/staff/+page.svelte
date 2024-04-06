bd2db82337325998b683cbecba91815bb29fef46
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-04-06 09:35:16 +0200

    just testing out zed-preview, but also starting up the admin/staff section

--- src/routes/(dashboard)/admin/staff/+page.svelte
+++ src/routes/(dashboard)/admin/staff/+page.svelte
@@ -1,1 +1,17 @@
+<script lang="ts">
+	export let data;
+	const { people } = data;
+</script>
+
 <h3>staff</h3>
+{#if people.length > 0}
+	{#each data.people as person}
+		<dl>
+			<dt>{person.name}</dt>
+			<dd>{person.status.state}</dd>
+			<dd>{person.role?.name}</dd>
+		</dl>
+	{/each}
+{:else}
+	<p>No staff found</p>
+{/if}
