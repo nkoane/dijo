f82851b10469692db60e5d3620de13091b035ab5
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-04-12 07:09:44 +0200

    we have working profile page, i am procastinating here

--- src/routes/(dashboard)/profile/+page.svelte
+++ src/routes/(dashboard)/profile/+page.svelte
@@ -1,1 +1,39 @@
-<h1>Profile</h1>
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	const account = data.user;
+</script>
+
+<h2>You</h2>
+
+<div id="profile">
+	<dl id="details">
+			<dt>{account?.username}</dt>
+			<dd>{account?.roleId}</dd>
+			<dd>{account?.stateId}</dd>
+	</dl>
+	<form action="">
+		<p>
+			<label for="username">Change your the username?</label>
+			<input type="text" id="username" />
+			<span>
+					 error
+				</span>
+				
+		</p>
+	</form>
+</div>
+
+<style lang="postcss">
+	div#profile {
+		dl.details {
+			@apply bg-red-100 p-4;
+		}
+
+		form {
+			@apply bg-yellow-4;
+		}
+	}
+
+</style>
