a11f960c64ddbde1adf64d27c54517b3cb3e867d
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-05-11 14:02:21 +0200

    not sure what, but i think neovim testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <h2 class="mb-4 text-3xl font-bold">
-	Hello, {data.user?.username ?? 'Stranger'}!
+	Hello, {data.user?.username ?? 'Stranger'}?
 </h2>
 
 <style lang="postcss">
-	h1 {
+	h2 {
 		@apply mb-4 text-3xl font-bold;
 	}
 </style>
